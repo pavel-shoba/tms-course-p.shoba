1ebe30297a6f425214f7c9fa238b90c37139cbc5
@pavel.shoba <pavel.shoba@neo.gg>
2024-12-08 16:02:11 +0300

    hometask for lesson 4 - task one

---
+++ src/hometasks/lesson4/TaskOne.java
@@ -0,0 +1,29 @@
+package hometasks.lesson4;
+
+public class TaskOne {
+    public static void main(String[] args) {
+
+        int [] first = {2, 61, 23, 99, 53, 111, 576, 5};
+        int [] second = {90, 5, 303, 765, 111, 32, 8, 14, 23, 202};
+
+        if (first.length > second.length) {
+            System.out.println("Repeated elements in both arrays: ");
+            for (int i = 0; i < first.length; i++) {
+                for (int j = 0; j < second.length; j++) {
+                    if (second[j] == first[i]) {
+                        System.out.println(first[i]);
+                    }
+                }
+            }
+        } else {
+            System.out.println("Repeated elements in both arrays: ");
+            for (int i = 0; i < second.length; i++) {
+                for (int j = 0; j < first.length; j++) {
+                    if (second[i] == first[j]) {
+                        System.out.println(second[i]);
+                    }
+                }
+            }
+        }
+    }
+}
